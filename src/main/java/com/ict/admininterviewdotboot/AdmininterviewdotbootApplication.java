b49c819c170c50efd95889a4ba9281c1e065fd94
hagjun <73927385+hagjun@users.noreply.github.com>
2024-06-21 10:16:52 +0900

    0621

---
+++ src/main/java/com/ict/admininterviewdotboot/AdmininterviewdotbootApplication.java
@@ -0,0 +1,13 @@
+package com.ict.admininterviewdotboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmininterviewdotbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmininterviewdotbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ict/admininterviewdotboot/AdmininterviewdotbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.admininterviewdotboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmininterviewdotbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
