1203af9519b09a62c9cedd39c51937a5d6139ff7
hagjun <73927385+hagjun@users.noreply.github.com>
2024-07-05 16:47:58 +0900

    학준커밋

--- src/main/java/com/ict/admininterviewdotboot/vo/ReviewVO.java
+++ src/main/java/com/ict/admininterviewdotboot/vo/ReviewVO.java
@@ -4,5 +4,5 @@
 
     @Data
     public class ReviewVO {
-        private String r_idx, u_idx, r_writer, r_title, r_content, r_company, r_grade, r_regdate, active;
+        private String r_idx, u_idx, r_id, r_writer, r_title, r_content, r_company, r_grade, r_regdate, active;
     }
